9a457d34b6fe09e2077d110c41b5e6e3be793c51
msvairam <mutvairam@gmail.com>
2026-09-20 18:21:12 +0530

    JSX Concepts

--- Igniting_app/App.js
+++ Igniting_app/App.js
@@ -1,7 +1,39 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const header = React.createElement('h1', {}, 'Hello World from React1');
-const root = ReactDom.createRoot(document.getElementById('root'));
+ // React Element => Object => HtmlElement to render in Root
+// React Element => React Element JS Object => HtmlElement(render)
 
-root.render(header);
+//const header = React.createElement('h1', { id: 'heading' }, 'Hello World from React1');
+
+// console.log(header);
+
+// JSX - HTML Like or XML like syntax
+// JSX => React Element => React Element JS Object => HtmlElement(render)
+
+const jsxHeader1 = (
+    <h1>
+        Welcome to React2
+    </h1>
+)
+
+const jsxHeader = [
+(
+    <h1 id="heading" className="test" tabIndex="1">
+        Hello World from React
+        <span>App</span>
+    </h1>
+),
+(
+    <h1 id="heading" className="test" tabIndex="1">
+        Welcome to React
+    </h1>
+), jsxHeader1, <a href="/">Home Page</a>
+]
+
+console.log(jsxHeader);
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+// console.log(root);
+
+root.render(jsxHeader);
